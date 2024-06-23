74008a91250660be6400c53960c4671f0924a1a5
Johannes Nägele <johannes-naegele@web.de>
2024-06-23 17:53:05 +0200

    Include correct file

--- src/Sraffa.jl
+++ src/Sraffa.jl
@@ -7,6 +7,7 @@ export real_eigvals, compute_R, replace_with_zero, safe_divide
 export n_switches, n_reswitches, switch_cases, switch_info
 export compute_w, plot_wage_curves
 
+using Logging
 using LinearAlgebra
 using DataFrames
 using ElasticArrays
@@ -26,7 +27,7 @@ abstract type EnvelopeMethod end
 
 include("envelope/types.jl")
 include("envelope/lp.jl")
-include("envelope/binary.jl")
+include("envelope/piecewise.jl")
 include("envelope/vfz.jl")
 include("envelope/envelope.jl")
 
